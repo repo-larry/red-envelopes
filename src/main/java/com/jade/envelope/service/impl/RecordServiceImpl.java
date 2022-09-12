3e2e4645941f89026ab7e6ccde37044fa773e7ff
songyx <2250794569@qq.com>
2022-09-12 14:23:06 +0800

    红包信息异步记录

--- src/main/java/com/jade/envelope/service/impl/RecordServiceImpl.java
+++ src/main/java/com/jade/envelope/service/impl/RecordServiceImpl.java
@@ -2,6 +2,7 @@ package com.jade.envelope.service.impl;
 
 import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.json.JSONObject;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jade.envelope.domain.EnvelopeInfo;
 import com.jade.envelope.domain.EnvelopeRecord;
@@ -10,11 +11,11 @@ import com.jade.envelope.mapper.EnvelopeRecordMapper;
 import com.jade.envelope.service.IEnvelopeInfoService;
 import com.jade.envelope.service.IRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +26,12 @@ public class RecordServiceImpl extends ServiceImpl<EnvelopeRecordMapper, Envelop
     @Autowired
     private IEnvelopeInfoService iEnvelopeInfoService;
 
-    @Autowired
-    @Qualifier("redisTemplate")
+    @Resource(name = "JacksonRedisTemplate")
     private RedisTemplate redisTemplate;
 
-    @Override
-    public void startPool() {
+    public void envelopeDataSync() {
         List<EnvelopeInfo> envelopeInfoList = iEnvelopeInfoService.getEmptyRecord();
         if (envelopeInfoList.size() == 0) {
-            System.out.println("没有未被持久化的红包信息");
             return;
         }
 
@@ -44,12 +42,10 @@ public class RecordServiceImpl extends ServiceImpl<EnvelopeRecordMapper, Envelop
             List<TinyEnvelope> tinyEnvelopeList1 = getUnTapped(ei.getEnvelopeId());
             if (tinyEnvelopeList1.size() != 0) {
                 System.out.println("redis中查找到未被抢完的红包,id为" + ei.getEnvelopeId());
-                continue;
             }
 
             if (tinyEnvelopeList.size() == 0) {
-                System.out.println("redis中未查询到红包号为" + ei.getEnvelopeId() + "的红包,出错");
-                //将status置为0,表示红包已经过期
+                // 红包已经过期，status = 0
                 iEnvelopeInfoService.updateStatus(ei);
                 continue;
             }
@@ -61,29 +57,29 @@ public class RecordServiceImpl extends ServiceImpl<EnvelopeRecordMapper, Envelop
                 envelopeRecord.setEnvelopeId(ei.getEnvelopeId());
                 envelopeRecord.setNickName("testUser_" + te.getUid());
                 envelopeRecord.setUid(te.getUid());
-                envelopeRecord.setAmount(te.getAmount().intValue());
+                envelopeRecord.setAccount(te.getAccount().intValue());
                 envelopeRecord.setCreateTime(LocalDateTimeUtil.now());
-                baseMapper.insert(envelopeRecord);
-                totalAmount += te.getAmount();
-                totalNum += 1;
+                boolean exists = baseMapper.exists(Wrappers.<EnvelopeRecord>query().lambda().eq(EnvelopeRecord::getUid, te.getUid()).eq(EnvelopeRecord::getEnvelopeId, te.getEnvelopeId()));
+                if (!exists) {
+                    baseMapper.insert(envelopeRecord);
+                    totalAmount += te.getAccount();
+                    totalNum += 1;
+                }
             }
-            System.out.println("持久化到数据库完成，共插入" + tinyEnvelopeList.size() + "条红包数据");
+            System.out.println("红包" + tinyEnvelopeList.get(0).getEnvelopeId() + ",共完成" + tinyEnvelopeList.size() + "条红包数据更新");
             //更新发布的红包信息
             ei.setRemainingAmount(ei.getRemainingAmount() - totalAmount.intValue());
             ei.setRemainingNumber(ei.getRemainingNumber() - totalNum);
             ei.setUpdateTime(LocalDateTimeUtil.now());
             if (ei.getRemainingAmount() == 0 && ei.getRemainingNumber() == 0) {
-                //持久化到数据库后删除redis中的数据
+                // 持久化到数据库后删除redis中的数据
                 redisTemplate.delete("envelopeConsumedList_" + ei.getEnvelopeId());
                 redisTemplate.delete("envelopeConsumedMap_" + ei.getEnvelopeId());
-                //如果红包被抢完设置为不可用
+                // 如果红包被抢完设置为不可用
                 ei.setStatus(0);
             }
-            if (iEnvelopeInfoService.updateEnvelopeInfo(ei)) {
-                System.out.println("红包发布信息更新成功");
-            } else {
-                System.out.println("红包发布信息更新失败");
-            }
+            iEnvelopeInfoService.updateEnvelopeInfo(ei);
+
         }
     }
 
@@ -93,7 +89,6 @@ public class RecordServiceImpl extends ServiceImpl<EnvelopeRecordMapper, Envelop
         ListOperations<Serializable, Object> operations = redisTemplate.opsForList();
         List<Object> objects = operations.range(envelopeConsumedListKey, 0, -1);
         if (objects == null) {
-            System.out.println("未查询到此红包信息");
             return null;
         }
         for (Object o : objects) {
@@ -101,7 +96,7 @@ public class RecordServiceImpl extends ServiceImpl<EnvelopeRecordMapper, Envelop
             TinyEnvelope te = new TinyEnvelope();
             te.setUid(Integer.parseInt(jsonObject.get("uid").toString()));
             te.setEnvelopeId(Long.valueOf(jsonObject.get("envelopeId").toString()));
-            te.setAmount(Long.valueOf(jsonObject.get("amount").toString()));
+            te.setAccount(Long.valueOf(jsonObject.get("account").toString()));
             results.add(te);
         }
         return results;
@@ -118,7 +113,8 @@ public class RecordServiceImpl extends ServiceImpl<EnvelopeRecordMapper, Envelop
         for (Object o : objects) {
             TinyEnvelope st = new TinyEnvelope();
             JSONObject jsonObject = new JSONObject(o);
-            st.setAmount(Long.valueOf(jsonObject.get("amount").toString()));
+            st.setUid(Integer.parseInt(jsonObject.get("uid").toString()));
+            st.setAccount(Long.valueOf(jsonObject.get("account").toString()));
             st.setEnvelopeId(Long.valueOf(jsonObject.get("envelopeId").toString()));
             results.add(st);
         }
